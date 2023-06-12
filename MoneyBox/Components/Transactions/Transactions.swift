d3683a59b3a02ac2bba3a43ce1d332f658767022
wlankasper <asu.artur.smirnov@gmail.com>
2023-06-12 19:55:45 +0200

    update

--- MoneyBox/Components/Transactions/Transactions.swift
+++ MoneyBox/Components/Transactions/Transactions.swift
@@ -215,3 +215,34 @@ struct AddExpenseView: View {
         .padding([.leading, .top, .trailing])
     }
 }
+
+let transactionCollection: [Group] = [
+    .init(
+        separator: Separator(title: "Today"),
+        transactionsCollection: [
+            Transaction(icon: "🥃", amount: "18,50", category: "Bar", type: "expense"),
+            Transaction(icon: "✈️", amount: "250,00", category: "Travel", type: "expense")
+        ]
+    ),
+    .init(
+        separator: Separator(title: "04 April"),
+        transactionsCollection: [
+            Transaction(icon: "🛒", amount: "48,93", category: "Supermarket", type: "expense"),
+            Transaction(icon: "🎮", amount: "50,00", category: "Game", type: "expense"),
+            Transaction(icon: "🍽️", amount: "127,40", category: "Restaurant", type: "expense"),
+            Transaction(icon: "💸", amount: "2000,00", category: "Innova SPA", type: "income")
+        ]
+    ),
+    .init(
+        separator: Separator(title: "03 April"),
+        transactionsCollection: [
+            Transaction(icon: "🛍️", amount: "99,99", category: "Shopping", type: "expense"),
+            Transaction(icon: "🚕", amount: "25,00", category: "Transportation", type: "expense"),
+            Transaction(icon: "🏥", amount: "350,00", category: "Medical", type: "expense"),
+            Transaction(icon: "🏠", amount: "1200,00", category: "Housing", type: "expense"),
+            Transaction(icon: "💻", amount: "899,00", category: "Technology", type: "expense"),
+            Transaction(icon: "📚", amount: "30,00", category: "Education", type: "expense"),
+            Transaction(icon: "🎁", amount: "50,00", category: "Gifts", type: "expense")
+        ]
+    )
+]
--- MoneyBox/Storage/TransactionStore.swift
+++ MoneyBox/Storage/TransactionStore.swift
@@ -5,45 +5,10 @@ class TransactionStore {
     
     @AppStorage("RawTransactions") var transactions: [[String]] = []
     
-    let transactionCollection: [Group] = [
-        .init(
-            separator: Separator(title: "Today"),
-            transactionsCollection: [
-                Transaction(icon: "🥃", amount: "18,50", category: "Bar", type: "expense"),
-                Transaction(icon: "✈️", amount: "250,00", category: "Travel", type: "expense")
-            ]
-        ),
-        .init(
-            separator: Separator(title: "04 April"),
-            transactionsCollection: [
-                Transaction(icon: "🛒", amount: "48,93", category: "Supermarket", type: "expense"),
-                Transaction(icon: "🎮", amount: "50,00", category: "Game", type: "expense"),
-                Transaction(icon: "🍽️", amount: "127,40", category: "Restaurant", type: "expense"),
-                Transaction(icon: "💸", amount: "2000,00", category: "Innova SPA", type: "income")
-            ]
-        ),
-        .init(
-            separator: Separator(title: "03 April"),
-            transactionsCollection: [
-                Transaction(icon: "🛍️", amount: "99,99", category: "Shopping", type: "expense"),
-                Transaction(icon: "🚕", amount: "25,00", category: "Transportation", type: "expense"),
-                Transaction(icon: "🏥", amount: "350,00", category: "Medical", type: "expense"),
-                Transaction(icon: "🏠", amount: "1200,00", category: "Housing", type: "expense"),
-                Transaction(icon: "💻", amount: "899,00", category: "Technology", type: "expense"),
-                Transaction(icon: "📚", amount: "30,00", category: "Education", type: "expense"),
-                Transaction(icon: "🎁", amount: "50,00", category: "Gifts", type: "expense")
-            ]
-        )
-    ]
-    
     func appendTransaction(transaction: TransactionStd) {
         self.transactions.append(transaction.getRawTransaction())
     }
-    
-//    func getTransactionsRaw() {
-//
-//    }
-    
+
     func getTransactionComponents() -> [Group] {
         var data: [Group] = []
         
